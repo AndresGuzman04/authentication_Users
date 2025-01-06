bad63048d1ab327a79f8e27200e1d2897484c2a5
AndresGuzman04 <andr35guzman2001@gmail.com>
2025-01-06 15:57:59 -0600

    Add Logout

--- index.js
+++ index.js
@@ -59,7 +59,11 @@ app.post('/register', async (req, res) => {
     res.status(400).send(error.message)
   }
 })
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('access_token')
+    .json({ message: 'Logout successful' })
+})
 
 app.get('/protected', (req, res) => {
   const { user } = req.session
