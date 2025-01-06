d35b1cd677e49cb6b12556d3128338a7cfd4dbdf
AndresGuzman04 <andr35guzman2001@gmail.com>
2025-01-05 22:29:23 -0600

    Modified views Login/Register

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ app.post('/register', async (req, res) => {
 })
 app.post('/logout', (req, res) => {})
 
-app.get('/prtected', (req, res) => {})
+app.get('/protected', (req, res) => {
+  res.render('protected')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${port}`)
