6c360c0e7c12f7812ddd35a2111996f6527e0c6a
AndresGuzman04 <andr35guzman2001@gmail.com>
2025-01-06 15:54:20 -0600

    Add JWT

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+
+import { PORT, SECRET_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
 
 app.set('view engine', 'ejs')
+
 app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_KEY)
+    req.session.user = data
+  } catch {}
+  next() // seguir a la siguiente ruta o middleware
+})
 
 const port = process.env.port ?? 3000
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body
   try {
     const user = await UserRepository.login({ username, password })
-    res.send({ user })
+    const token = jwt.sign({ userId: user.id, username: user.username }, SECRET_KEY, {
+      expiresIn: '1h'
+    })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // la cookie solo se puede acceder en el servidor
+        secure: true, // solo se puede acceder en https
+        sameSite: 'strict', // solo se puede acceder en el mismo dominio
+        maxAge: 3600000 // la cookie expira en 1 hora
+      })
+      .send({ user, token })
   } catch (error) {
     res.status(401).send(error.message)
   }
@@ -35,7 +62,9 @@ app.post('/register', async (req, res) => {
 app.post('/logout', (req, res) => {})
 
 app.get('/protected', (req, res) => {
-  res.render('protected')
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access not authorized')
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
